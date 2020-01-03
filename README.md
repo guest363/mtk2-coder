c777a0de5a6484752dd0aafd4f4fdccb1bac5181
Oleg <guest363@gmail.com>
2020-01-03 17:16:52 +0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Кодер текста в МТК-2
+
+[Demo](https://guest363.github.io/)
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'docs')
+    },
+
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new HtmlWebpackPlugin({
+            hash: true,
+            filename: 'index.html',
+            template: 'index.html'
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /.(js|jsx)$/,
+                include: [path.resolve(__dirname, 'src')],
+                loader: 'babel-loader',
+
+                options: {
+                    plugins: ['syntax-dynamic-import'],
+
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                            {
+                                modules: false
+                            }
+                        ]
+                    ]
+                }
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader', // translates CSS into CommonJS
+                    },
+                    {
+                        loader: 'less-loader', // compiles Less to CSS
+                    },
+                ],
+            }
+        ]
+    },
+
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendors: {
+                    priority: -10,
+                    test: /[\\/]node_modules[\\/]/
+                }
+            },
+
+            chunks: 'async',
+            minChunks: 1,
+            minSize: 30000,
+            name: true
+        }
+    },
+
+    devServer: {
+        open: true
+    }
+};
